039fc4f606e382c7600f89d9d3d3afc373236d38
Timothy Pratley <timothypratley@gmail.com>
2015-12-04 23:47:08 -0800

    fixing deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
+lein clean
 lein cljsbuild once min
 cd resources/public
 git init
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "git:github.com/timothypratley/pirates.git" master:gh-pages
+git push --force --quiet "git@github.com:timothypratley/pirates.git" master:gh-pages
+rm -fr resources/public/.git
