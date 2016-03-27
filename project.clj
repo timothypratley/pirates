a36b9d419a2023b705758fd2222ee32134a2b9fd
Timothy Pratley <timothypratley@gmail.com>
2016-03-27 00:00:10 -0700

    min-lein version bump

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
+  :min-lein-version "2.6.1"
+
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.34"]
                  [org.clojure/core.async "0.2.374"]
